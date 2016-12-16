1545f236ab109899b98d8868a1328ce24d93ed18
Sragaai <sherif.ragaai@gmail.com>
2016-12-16 23:39:53 +0200

    First Commit

---
+++ TestEnv/main.c
@@ -0,0 +1,22 @@
+#ifndef NULL
+#define NULL 0
+#endif
+
+#include <Util.h>
+
+extern void SORT_INSERTION_TEST(void **state);
+extern void SORT_MERGE_TEST(void **state);
+
+int main(void) {
+
+	cmocka_set_message_output(CM_OUTPUT_TAP);
+
+	const struct CMUnitTest tests[] = {
+	cmocka_unit_test(SORT_INSERTION_TEST),
+	cmocka_unit_test(SORT_MERGE_TEST) };
+
+
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+
+}
